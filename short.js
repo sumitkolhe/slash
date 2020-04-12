e3249792f1df630b11e16e82c27178d8842b4a8b
Sumit kolhe <thesumitkolhe@gmail.com>
2020-04-12 12:18:20 +0530

    fixed refresh on enter

--- short.js
+++ short.js
@@ -1,7 +1,5 @@
 const endpoint = "https://jsonbox.io/demobox_6d9e326c183fde7b"
-
-
-
+new ClipboardJS('#copyto');
 
 new Vue({
     el: '#app',
@@ -29,7 +27,7 @@ new Vue({
             .then(function(response){
             var redirecturl = response.data[0].link;
             console.log(redirecturl);
-           window.location.assign(redirecturl);
+           window.location.href= redirecturl;
             });                
 
             }
@@ -42,16 +40,16 @@ new Vue({
             this.urlhash = Math.random().toString(36).substring(9);
             this.finalurl = this.reduced+"/#"+this.urlhash;
             console.log(this.finalurl);
-            this.longurl=this.checkurl();
+            this.longurl=this.checkurl(this.url);
             this.posturl(this.urlhash,this.longurl)
-
+            console.log(this.longurl);
            
             }
         },
 
 
         checkurl(url){  
-            var pattern = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+            var pattern = /(http|https|ftp):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/
         if (pattern.test(this.url)) {
             return this.url;
         } 
