0472e8e7e8771f3d7254ad2a2600af71c6c6edc1
Sumit kolhe <thesumitkolhe@gmail.com>
2020-04-14 20:54:02 +0530

    trial

--- redirect.js
+++ redirect.js
@@ -1,9 +1,13 @@
 
-const endpoint = 'https://jsonbox.io/demobox_6d9e326c183fde7b';
+const endpoint = "https://jsonbox.io/demobox_6d9e326c183fde7b";
 var windowurl = window.location.hash.substr(1);
 
-if (Window.location.hash!=""){     
-    let response = await fetch(endpoint+'?q=hash:'+windowurl);
-    let data = await response.json(); // read response body and parse as JSON
+if (window.location.hash!=""){
+    
+    
+fetch(endpoint+'?q=hash:'+windowurl)
+.then((resp) => resp.json()) // Transform the data into json
+.then(function(data) {
     window.location.href=data[0].link;
+  })
 }
