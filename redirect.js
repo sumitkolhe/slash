86f358211daa1b82e770d955da61666d05e836b7
Sumit kolhe <thesumitkolhe@gmail.com>
2020-04-14 18:57:58 +0530

    fixes

--- redirect.js
+++ redirect.js
@@ -1,12 +1,13 @@
 const endpoint = "https://jsonbox.io/demobox_6d9e326c183fde7b";
       
+var windowurl = window.location.hash.substr(1);
     if(window.location.hash!="" ){
-        var windowurl = window.location.hash.substring(1);
+        
         console.log(endpoint+'?q=hash:'+windowurl);
         axios.get(endpoint+'?q=hash:'+windowurl)
         .then(function(response){
         var redirecturl = response.data[0].link;
         console.log(redirecturl);
-        window.location= redirecturl;
+        window.location.href= redirecturl;
         });                
         }
